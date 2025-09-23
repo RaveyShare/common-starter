8a2b20018c02aaf2bf517f51042f892937ad9200
ravey <15132698828@163.com>
2025-09-23 08:01:51 +0800

    修改 MybatisPlusConfig 配置

--- common-dao/common-dao-mp/src/main/java/com/ravey/common/dao/mp/config/MybatisPlusConfig.java
+++ common-dao/common-dao-mp/src/main/java/com/ravey/common/dao/mp/config/MybatisPlusConfig.java
@@ -18,10 +18,12 @@ import org.apache.ibatis.reflection.wrapper.ObjectWrapperFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(RaveyMybatisPlusProperties.class)
 public class MybatisPlusConfig {
     @Autowired
     private RaveyMybatisPlusProperties properties;
